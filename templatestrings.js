f41d612517c11ddc24d8cc75f4ad7dd153156f31
Ana Carolyne Pereira de Souza <ana.pereira@tecnospeed.com.br>
2022-08-18 16:34:15 -0300

    subindo mais code

---
+++ templatestrings.js
@@ -0,0 +1,10 @@
+const nome = "ana";
+const idade = 2022-2001;
+
+const cidadedenascimento = "sarandi";
+//const apresentacao = "meu nome é" + nome + ", minha idade é" + idade + "e nasci na cidade de " + cidadedenascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadedenascimento} `; // template string ou so strings
+
+console.log (apresentacao)
+
---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idademinima = 18;
+const idadecliente = 16; 
+//    if (idadecliente>= idademinima){
+//        console.log ("cerveja")
+//    }
+//    else {
+//        console.log ("suco")
+//    }
+
+    console.log(idadecliente >= idademinima ?"cerveja": "suco") //complica a leitura, boa pratica é usar if e else
