e8f506fe3cae14777dda7ae91d78807bbc332266
Ana Carolyne Pereira de Souza <ana.pereira@tecnospeed.com.br>
2022-08-22 10:32:29 -0300

    mais codigos do alura

---
+++ adicionandonota.js
@@ -0,0 +1,7 @@
+const notas = [10, 6, 8] // 7 - ultimo elemento pra passar pro push
+notas.push(7)
+console.log(notas)
+
+let media = (notas [0] + notas[1] + notas [2] +notas [3] ) /notas.length
+
+console.log (media)
